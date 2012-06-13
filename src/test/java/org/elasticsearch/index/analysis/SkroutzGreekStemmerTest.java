29dec0641bbb58cceb4fab74395425b089921566
Tasos Stathopoulos <stathopa@skroutz.gr>
2012-06-13 12:15:11 +0300

    Tests for SkroutzGreekStemmer

---
+++ src/test/java/org/elasticsearch/index/analysis/SkroutzGreekStemmerTest.java
@@ -0,0 +1,43 @@
+package org.elasticsearch.index.analysis;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SkroutzGreekStemmerTest {
+  private final SkroutzGreekStemmer stemmer = new SkroutzGreekStemmer();
+
+  /**
+   * a sample of greek words that should be stemmed by the Skroutz Greek stemmer.
+   */
+  private static final String[] words = { "προσκλητηριο", "προσκλητηρια",
+      "βιντεοπροβολεασ", "βιντεοπροβολεισ", "αγριοσ", "αγριο", "αγριουσ",
+      "αγροσ", "γιοσ", "γαλαζιοι", "γαλαζια", "κουρεασ", "κουρεα", "αμυνοξυ",
+      "αμυνοξεα", "αμυνοξεων", "αγιασ", "αγων", "παπουτσια", "παπουτσι",
+      "γραμματοκιβωτιο", "γραμματοκιβωτια", "παρεα", "παρεο", "παρεεσ",
+      "στερεεσ", "στερεοσ", "στερεα", "φαση", "φασεωσ"};
+
+  /**
+   * the stems that should be returned by the stemmer for the above words.
+   */
+  private static final String[] stems = { "προσκλητηρ", "προσκλητηρ",
+      "βιντεοπροβολ", "βιντεοπροβολ", "αγρι", "αγρι", "αγρι", "αγρ", "γι",
+      "γαλαζ", "γαλαζ", "κουρ", "κουρ", "αμυνοξ", "αμυνοξ", "αμυνοξ", "αγι",
+      "αγ", "παπουτσ", "παπουτσ", "γραμματοκιβωτ", "γραμματοκιβωτ", "παρε",
+      "παρε", "παρε", "στερε", "στερε", "στερε", "φασ", "φασ"};
+
+  private char[] token;
+  private String stem;
+  private int tokenLength, stemLength;
+
+  @Test
+  public void testSkroutzGreekStemmer() {
+    for (int i = 0; i < words.length; i++) {
+      token = words[i].toCharArray();
+      tokenLength = words[i].length();
+      stemLength = stemmer.stem(token, tokenLength);
+      stem = new String(token, 0, stemLength);
+
+      Assert.assertEquals(stem, stems[i]);
+    }
+  }
+}
