b1177469d216dc447d738e714f8fd85f6aba6471
Tasos Stathopoulos <stathopa@skroutz.gr>
2013-05-23 14:14:57 +0300

    Minor reformat of the binder

--- src/main/java/org/elasticsearch/index/analysis/SkroutzGreekStemmerBinderProcessor.java
+++ src/main/java/org/elasticsearch/index/analysis/SkroutzGreekStemmerBinderProcessor.java
@@ -4,7 +4,8 @@ import org.elasticsearch.index.analysis.AnalysisModule.AnalysisBinderProcessor;
 
 public class SkroutzGreekStemmerBinderProcessor extends AnalysisBinderProcessor {
 	
-	@Override public void processTokenFilters(TokenFiltersBindings tokenFiltersBindings) {
+	@Override
+	public void processTokenFilters(TokenFiltersBindings tokenFiltersBindings) {
 		tokenFiltersBindings.processTokenFilter("skroutz_stem_greek", SkroutzGreekStemmerTokenFilterFactory.class);
 	}
 
