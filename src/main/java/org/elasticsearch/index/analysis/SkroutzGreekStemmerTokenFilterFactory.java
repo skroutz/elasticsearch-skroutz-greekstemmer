4a6b70882289441418538e9a4ea6430f34f76c0f
Bill Kolokithas <kolokithas.b@gmail.com>
2016-04-24 22:33:22 +0300

    Remove version argument from lucene analyzers
    Lucene has deprecated the version argument in the analyzers
    constructor & methods.
    In lucene 5.5 which is used by elasticsearch 2.3, support for the
    argument is completely removed.
    
    See: https://github.com/apache/lucene-solr/commit/cb9beac70790e460ce5277d6adc2e36b703d3270

--- src/main/java/org/elasticsearch/index/analysis/SkroutzGreekStemmerTokenFilterFactory.java
+++ src/main/java/org/elasticsearch/index/analysis/SkroutzGreekStemmerTokenFilterFactory.java
@@ -3,22 +3,23 @@ package org.elasticsearch.index.analysis;
 import java.io.IOException;
 
 import org.apache.lucene.analysis.TokenStream;
+
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.assistedinject.Assisted;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.Index;
-import org.elasticsearch.index.settings.IndexSettings;
+import org.elasticsearch.index.settings.IndexSettingsService;
 
 public class SkroutzGreekStemmerTokenFilterFactory extends
 		AbstractTokenFilterFactory {
 
 	@Inject
 	public SkroutzGreekStemmerTokenFilterFactory(Index index,
-			@IndexSettings Settings indexSettings,
+			IndexSettingsService indexSettings,
 			Environment env, @Assisted String name,
 			@Assisted Settings settings) throws IOException {
-		super(index, indexSettings, name, settings);
+		super(index, indexSettings.getSettings(), name, settings);
 	}
 
 	@Override
