b9ed298d083032b8ba0168474a4e75d3c7233f2e
Bill Kolokithas <kolokithas.b@gmail.com>
2017-01-27 00:52:29 +0200

    Move from dynamic settings to full-fledged class
    As a result, the 'index' argument from the constructor is dropped (and
    so is the import).
    
    You can find the reasoning behind this change in the description of the
    following commit:
    https://github.com/elastic/elasticsearch/commit/66d5d0c4f2de78371ccb5447e503d9d30b6b51c4

--- src/main/java/org/elasticsearch/index/analysis/SkroutzGreekStemmerTokenFilterFactory.java
+++ src/main/java/org/elasticsearch/index/analysis/SkroutzGreekStemmerTokenFilterFactory.java
@@ -7,13 +7,13 @@ import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.assistedinject.Assisted;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
-import org.elasticsearch.index.settings.IndexSettingsService;
+import org.elasticsearch.index.IndexSettings;
 
 public class SkroutzGreekStemmerTokenFilterFactory extends
 		AbstractTokenFilterFactory {
 
 	@Inject
-	public SkroutzGreekStemmerTokenFilterFactory(IndexSettingsService indexSettings,
+	public SkroutzGreekStemmerTokenFilterFactory(IndexSettings indexSettings,
 												 Environment env, @Assisted String name,
 												 @Assisted Settings settings) throws IOException {
 		super(indexSettings, name, settings);
