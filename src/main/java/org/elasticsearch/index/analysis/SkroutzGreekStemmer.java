1719d6e1be910d85daf5f56fbacf30fe3fa36670
Tasos Stathopoulos <stathopa@skroutz.gr>
2025-07-25 12:53:01 +0300

    Remove unused logging imports from SkroutzGreekStemmer

--- src/main/java/org/elasticsearch/index/analysis/SkroutzGreekStemmer.java
+++ src/main/java/org/elasticsearch/index/analysis/SkroutzGreekStemmer.java
@@ -5,9 +5,6 @@ import java.util.Arrays;
 import org.apache.lucene.analysis.el.GreekLowerCaseFilter;
 import org.apache.lucene.analysis.CharArraySet;
 
-import org.apache.logging.log4j.Logger;
-import org.elasticsearch.common.logging.Loggers;
-
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -56,8 +53,6 @@ import org.elasticsearch.common.logging.Loggers;
  * about these suffixes are added.
  */
 public class SkroutzGreekStemmer {
-  protected final Logger logger =
-          Loggers.getLogger(SkroutzGreekStemmer.class, SkroutzGreekStemmer.class.getSimpleName());
 
   public SkroutzGreekStemmer() { }
 
